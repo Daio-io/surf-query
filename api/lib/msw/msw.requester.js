a36d1c034b8186a552c25103801deb694e389901
Dai <dai.williams@bbc.co.uk>
2015-05-03 00:50:27 +0100

    Added library files and started building up request query

---
+++ api/lib/msw/msw.requester.js
@@ -0,0 +1,9 @@
+var MswClient = require('./msw.client');
+
+exports.makeRequest = function(_spotId) {
+    
+    MswClient.setSpotId(_spotId);
+    
+    // Todo: Make request and return data
+    
+};
--- api/surfify/surfify.handlers.js
+++ api/surfify/surfify.handlers.js
@@ -2,9 +2,23 @@
 
 exports.getSurfForecast = function *() {
 
+    let queries = this.request.query;
+
+    let spotId = queries.spotid;
+    let start = queries.start;
+    let end = queries.end;
+    let maxWind = queries.maxwind;
+    let minSwell = queries.minswell;
+    
     this.body = {
         
-        response: 'placeholder response'
+        // Placeholder to test out query
+        response: 'placeholder response',
+        spotid: spotId,
+        start: start,
+        end: end,
+        maxwind: maxWind,
+        minswell: minSwell
         
     }
 
