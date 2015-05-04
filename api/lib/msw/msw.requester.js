19cc81772be2ccce99e0badf819b40170f7f5b2c
Dai <dai.williams@bbc.co.uk>
2015-05-04 18:10:38 +0100

    Refactored response into a private helper function

--- api/lib/msw/msw.requester.js
+++ api/lib/msw/msw.requester.js
@@ -4,34 +4,46 @@ var MswClient = require('./msw.client');
 
 // TODO: logic to only include data specified in the notification poll
 exports.makeRequest = function(_spotId) {
-    
+
     MswClient.setSpotId(_spotId);
-    
-    return MswClient.exec().then(function(data){
-        
-        // Map response data to only take important data for Surfify
-        let response = data.map(function(item){
-            
-            return {
 
-                timestamp: item.timestamp,
-                wind: item.wind.speed,
-                weather: item.condition.weather + item.condition.unit,
-                minSwell: item.swell.minBreakingHeight,
-                maxSwell: item.swell.maxBreakingHeight,
-                solidStar: item.solidRating,
-                fadedStar: item.fadedRating
-                
-            }
-            
+    return MswClient.exec()
+
+        .then(_buildResponse)
+
+        .catch(function(err) {
+
+            return err;
+
         });
-        
-        return response;
 
-    }).catch(function(err) {
-        
-        return err;
-        
-    });
-        
 };
+
+/**
+ * 
+ * @param _data
+ * @returns {*}
+ * @private
+ */
+function _buildResponse(_data) {
+
+    // Map response data to only take important data for Surfify
+    let response = _data.map(function (item) {
+
+        return {
+
+            timestamp: item.timestamp,
+            wind: item.wind.speed,
+            weather: item.condition.weather + item.condition.unit,
+            minSwell: item.swell.minBreakingHeight,
+            maxSwell: item.swell.maxBreakingHeight,
+            solidStar: item.solidRating,
+            fadedStar: item.fadedRating
+
+        }
+
+    });
+
+    return response;
+
+}
