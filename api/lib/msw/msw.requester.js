9f36f9797af8079e412b92bc4faef700d98e4297
Dai <dai.williams@bbc.co.uk>
2015-05-04 18:04:03 +0100

    Testing out maping data response into a custom response

--- api/lib/msw/msw.requester.js
+++ api/lib/msw/msw.requester.js
@@ -1,9 +1,37 @@
+'use strict';
+
 var MswClient = require('./msw.client');
 
+// TODO: logic to only include data specified in the notification poll
 exports.makeRequest = function(_spotId) {
     
     MswClient.setSpotId(_spotId);
     
-    // Todo: Make request and return data
-    
+    return MswClient.exec().then(function(data){
+        
+        // Map response data to only take important data for Surfify
+        let response = data.map(function(item){
+            
+            return {
+
+                timestamp: item.timestamp,
+                wind: item.wind.speed,
+                weather: item.condition.weather + item.condition.unit,
+                minSwell: item.swell.minBreakingHeight,
+                maxSwell: item.swell.maxBreakingHeight,
+                solidStar: item.solidRating,
+                fadedStar: item.fadedRating
+                
+            }
+            
+        });
+        
+        return response;
+
+    }).catch(function(err) {
+        
+        return err;
+        
+    });
+        
 };
--- api/surfify/surfify.handlers.js
+++ api/surfify/surfify.handlers.js
@@ -1,25 +1,17 @@
 'use strict';
 
+var mswRequest = require('../lib/msw/msw.requester');
+
 exports.getSurfForecast = function *() {
 
     let queries = this.request.query;
 
-    let spotId = queries.spotid;
+    let spotId = parseInt(queries.spotid);
     let start = queries.start;
     let end = queries.end;
     let maxWind = queries.maxwind;
     let minSwell = queries.minswell;
-    
-    this.body = {
-        
-        // Placeholder to test out query
-        response: 'placeholder response',
-        spotid: spotId,
-        start: start,
-        end: end,
-        maxwind: maxWind,
-        minswell: minSwell
-        
-    }
+
+    this.body = yield mswRequest.makeRequest(spotId);
 
 };
