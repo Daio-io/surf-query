8c6a1910e5d74552a5880ff12274b73cd651ac32
Dai <dai.williams@bbc.co.uk>
2015-07-25 09:05:02 +0100

    temporary caching solution added

--- api/lib/msw/msw.requester.js
+++ api/lib/msw/msw.requester.js
@@ -1,86 +1,106 @@
 'use strict';
 
-var MswClient = require('./msw.client');
-var moment = require('moment');
+const NodeCache = require('node-cache');
+const THREE_HOURS = 10800000;
 
-exports.makeRequest = function (_spotId, _maxWind, _minSwell, start, end) {
+const moment = require('moment');
+const tooly = require('tooly');
+
+var mswCache = new NodeCache();
+var MswClient = require('./msw.client');
+
+exports.makeRequest = function(_spotId, _maxWind, _minSwell, start, end) {
+
+  let cached = mswCache.get(_spotId);
+  
+  if (tooly.existy(cached)) {
+
+    return new Promise(function(resolve) {
+
+      resolve(_processRequest(cached, _maxWind, _minSwell, start, end))
+
+    });
+
+  } else {
 
     MswClient.setSpotId(_spotId);
 
     return MswClient.exec()
 
-        .then(function (data) {
+      .then(function(data) {
 
-            if(isNaN(start) || isNaN(end)) {
+        mswCache.set(_spotId, data, THREE_HOURS);
 
-                return _buildResponse(data, _maxWind, _minSwell, 0, 24);
+        return _processRequest(data, _maxWind, _minSwell, start, end)
 
-            } else {
+      })
 
-                return _buildResponse(data, _maxWind, _minSwell, start, end);
+      .catch(function(err) {
 
-            }
+        return err;
 
-        })
-
-        .catch(function (err) {
-
-            return err;
-
-        });
+      });
+  }
 
 };
+
+function _processRequest(data, _maxWind, _minSwell, start, end) {
+
+  if (isNaN(start) || isNaN(end)) {
+
+    return _buildResponse(data, _maxWind, _minSwell, 0, 24);
+
+  } else {
+
+    return _buildResponse(data, _maxWind, _minSwell, start, end);
+
+  }
+
+}
 
 /**
  *
  * @param _data
+ * @param _wind
+ * @param _minSwell
+ * @param start
+ * @param end
  * @returns {*}
  * @private
  */
 function _buildResponse(_data, _wind, _minSwell, start, end) {
 
-    // Map response data to only take important data for Surfify
-    let response = _data.map(function (item) {
+  // Map response data to only take important data for Surfify
+  let response = _data.map(function(item) {
 
-        let time = moment.unix(item.timestamp).format('H');
-        let timey = parseInt(time);
+    let time = moment.unix(item.timestamp).format('H');
+    let timey = parseInt(time);
 
-        if (item.wind.speed < _wind && 
-            item.swell.minBreakingHeight > _minSwell &&
-            timey >= start &&
-            timey <= end) {
+    if (item.wind.speed < _wind &&
+      item.swell.minBreakingHeight > _minSwell &&
+      timey >= start &&
+      timey <= end) {
 
-            return {
+      return {
 
-                timestamp: item.timestamp,
-                date: moment.unix(item.timestamp).format('MMMM Do YYYY'),
-                time: moment.unix(item.timestamp).format('H:MM'),
-                wind: item.wind.speed,
-                weather: item.condition.weather + item.condition.unit,
-                minSwell: item.swell.minBreakingHeight,
-                maxSwell: item.swell.maxBreakingHeight,
-                solidStar: item.solidRating,
-                fadedStar: item.fadedRating
+        timestamp: item.timestamp,
+        date: moment.unix(item.timestamp).format('MMMM Do YYYY'),
+        time: moment.unix(item.timestamp).format('H:MM'),
+        wind: item.wind.speed,
+        weather: item.condition.weather + item.condition.unit,
+        minSwell: item.swell.minBreakingHeight,
+        maxSwell: item.swell.maxBreakingHeight,
+        solidStar: item.solidRating,
+        fadedStar: item.fadedRating
 
-            }
+      }
 
-        }
-        else return null;
+    }
+    else return null;
 
-    });
+  });
 
-    return response.filter(_filterEmptyValues);
+  // Filter out all values that are null and undefined
+  return response.filter(tooly.existy);
 
-}
-
-/**
- * *
- * @param item
- * @returns {boolean}
- * @private
- */
-function _filterEmptyValues(item) {
-    
-    return item !== null;
-    
 }
