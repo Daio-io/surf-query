9c2f13cc36dc3497a91c8323116f5e0267167d15
Daio-io <dai.williams@bbc.co.uk>
2016-03-27 19:23:25 +0100

    - Ensure no cache on status page

--- api/status/status.handlers.js
+++ api/status/status.handlers.js
@@ -1,7 +1,8 @@
 'use strict';
 
 exports.getStatus = function *() {
-    
+  this.set("Cache-Control", "no-cache, no-store, must-revalidate");
+  this.set("Pragma", "no-cache");
+  this.set("Expires", "0");
   this.body= {status: 'OK'};
-    
 };
